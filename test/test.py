f559a27cac5aa1fe89ba8c0664f6a56ce8c587df
niraj yadav <niraj19082002@gmail.com>
2026-09-19 22:25:16 +0530

    in ci pipeline test updated

--- test/test.py
+++ test/test.py
@@ -1,10 +1,9 @@
-from langchain.agents import AgentState
-from src.agent_graph.graph_workflow import graph
+import pytest
 from src.vectordb.load_vectordb import retriever
 
 def test_retrieval_top_k():
     docs = retriever.invoke(
-        "What is the refund period?"
+        "What is RAG?",
     )
 
     assert len(docs) <= 5
